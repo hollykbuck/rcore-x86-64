ec2029cd1f691c391efca32fb87cca47f031b7cf
hollykbuck <hollykbuck@users.noreply.github.com>
2026-08-02 12:38:07 +0000

    deploy: 64846d13184dd0f805ae9976a375f130d95e3eb5

---
+++ ch1/src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["os",["",[],["console.rs","lang_items.rs","limine_reqs.rs","logging.rs","main.rs","uart.rs"]]]]');
+//{"start":19,"fragment_lengths":[95]}
---
+++ ch1/trait.impl/core/marker/trait.Unpin.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"os/console/struct.Stdout.html\" title=\"struct os::console::Stdout\">Stdout</a>",0,1,["os::console::Stdout"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"os/logging/struct.SimpleLogger.html\" title=\"struct os::logging::SimpleLogger\">SimpleLogger</a>",0,1,["os::logging::SimpleLogger"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[600]}
